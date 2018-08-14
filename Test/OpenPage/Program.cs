2ece3ed4e2098e94d29b3f2bd21ce299f0d16ac2
HAVG <HAVG@HAVG-PC>
2018-08-14 23:14:36 +0300

    Edited OpenPage method in view of notes. And Created method of PageOpening.

--- Test/OpenPage/Program.cs
+++ Test/OpenPage/Program.cs
@@ -8,6 +8,31 @@ namespace OpenPage
 {
     public class Program
     {
+        // сначала думал перенести инициализацию списка в этот метод,
+        // но потом подумал, что ему здесь не место
+        // или все таки он здесь должен быть?
+        private static void PageOpening(List<string> listOfEntities, int numberOfPage)
+        {
+            const int numberOfElementsOnPage = 2;
+            var indexOfFirstElement = numberOfPage * numberOfElementsOnPage - numberOfElementsOnPage;
+            var indexOfLastElement = numberOfPage * numberOfElementsOnPage;
+
+            Console.WriteLine($"Page {numberOfPage}:");
+            for (int i = indexOfFirstElement; i < indexOfLastElement; i++)
+            {
+                if (i < 0 || i >= listOfEntities.Count)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine($">{listOfEntities[i]}");
+                }
+            }
+            Console.WriteLine("End of page");
+
+        }
+
         public static void Main(string[] args)
         {
             var listOfEntities = new List<string>
@@ -20,34 +45,16 @@ namespace OpenPage
                 "sixth",
                 "seventh"
             };
-            const int numberOfElementsOnPage = 2;
             string stringFromUser;
-            int numberOfPage;
-
             Console.WriteLine("Type number of page and press enter or type \"exit\" to close application");
             do
             {
                 stringFromUser = Console.ReadLine();
-                if (Int32.TryParse(stringFromUser, out numberOfPage) && Convert.ToInt32(stringFromUser) >= 0)
+                //int numberOfPage;  // правильно будет если эту переменную задам сразу в TryParse?
+                if (Int32.TryParse(stringFromUser, out int numberOfPage) && numberOfPage > 0)
                 {
                     numberOfPage = Convert.ToInt32(stringFromUser);
-                    var indexOfFirstElement = numberOfPage * numberOfElementsOnPage - numberOfElementsOnPage;
-                    var indexOfLastElement = numberOfPage * numberOfElementsOnPage;
-
-                    Console.WriteLine($"Page {numberOfPage}:");
-                    for (int i = indexOfFirstElement; i < indexOfLastElement; i++)
-                    {
-                        if (i < 0 || i >= listOfEntities.Count)
-                        {
-                            break;
-                        }
-                        else
-                        {
-                            Console.WriteLine($">{listOfEntities[i]}");
-                        }
-                    }
-                    Console.WriteLine("End of page");
-
+                    PageOpening(listOfEntities, numberOfPage);
                 }
                 else if (stringFromUser == "exit")
                 {
@@ -60,7 +67,6 @@ namespace OpenPage
 
                 Console.WriteLine();
             } while (stringFromUser != "exit");
-
         }
     }
 }
